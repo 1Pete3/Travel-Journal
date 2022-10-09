b5be6224335e0a63873d09293c53b92e4e1974cd
1pete3 <skowronekpeter103@gmail.com>
2022-10-09 12:30:31 -0400

    Updated styling to use bootstrap and react bootstrap, fixed responsiveness

--- src/components/locations.js
+++ src/components/locations.js
@@ -1,28 +1,38 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export default function Location(props) {
   return (
-    <div className="location">
-      <div class="image">
-        <img src={props.x.imageUrl} alt="Location" className="locationImage"></img>
-      </div>
-
-      <div className="text">
-        <span>
-          <p className="locationName">
-            <FontAwesomeIcon icon={faLocationDot} className="locationDot" />
-            {props.x.location}
+    <Container>
+      <Row className="my-3 border-bottom border-danger border-2">
+        <Col sm={12} lg={6}  className=" d-flex align-items-center justify-content-center">
+          <img src={props.x.imageUrl} alt="Location" className="w-75"></img>
+        </Col>
+        <Col sm={12} lg={6} className="p-3 text-center">
+            <p className="text-uppercase">
+              <FontAwesomeIcon icon={faLocationDot} className="text-danger" /> &nbsp;
+              {props.x.location} 
+            </p>
+            <a href={props.x.googleMapsUrl} target="_new">
+              View on Google Maps
+            </a>
+            &nbsp;
+            <img
+              src={props.x.flag}
+              alt="Country flag"
+              className="countryFlag
+"
+            ></img>
+         
+          <h1>{props.x.title}</h1>
+          <p>
+            {props.x.startDate} - {props.x.endDate}
           </p>
-          <a className="locationMapLink" href={props.x.googleMapsUrl} target="_new">
-            View on Google Maps <img src={props.x.flag} alt="Country flag" className="countryFlag"></img>
-          </a>
-        </span>
-        <h1 className="locationTitle">{props.x.title}</h1>
-        <p className="date">
-          {props.x.startDate} - {props.x.endDate}
-        </p>
-        <p>{props.x.description}</p>
-      </div>
-    </div>
+          <p>{props.x.description}</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEarthAmerica } from '@fortawesome/free-solid-svg-icons';
 export default function NavBar() {
   return (
-    <nav>
-      <h2 className="JournalTitle">
-        <FontAwesomeIcon icon={faEarthAmerica} className="EarthIcon" />
+    <nav className='bg-danger p-1'>
+      <h2 className="text-center text-light">
+        <FontAwesomeIcon icon={faEarthAmerica}/>
+        &nbsp;
         My Travel Journal
       </h2>
     </nav>
